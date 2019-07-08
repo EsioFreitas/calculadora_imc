350de76d357d5ece964e4c3fc2fd9270f4a33532
EsioFreitas <esio.gustavo@gmail.com>
2019-07-08 10:25:54 -0300

    Add inputs

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,30 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-          )
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline, size: 120, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (Cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25),
+          ),
         ],
       ),
     );
